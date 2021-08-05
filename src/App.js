ca5d0e9e0939114bc73b48126cb54c66181dac64
dev001 <>
2021-08-05 23:47:49 +0530

    fix:node-sass semver issue

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Carousel from "./components/Carousel/Carousel";
+import Header from "./components/Header/Header";
+import Routes from "./Routes/Routes";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
